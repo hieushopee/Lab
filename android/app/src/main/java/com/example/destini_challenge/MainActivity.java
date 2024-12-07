077ce05a4156d981cd2e4989edbe34d7404a4533
hieushopee <97105402+hieushopee@users.noreply.github.com>
2024-12-07 12:00:08 +0700

    Delete android directory

--- android/app/src/main/java/com/example/destini_challenge/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.destini_challenge;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/com/example/destiny_challenge/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.destiny_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
